9560ec7a7d51904319c69a5c714abacf4ad8a205
csdj92 <csdj92@gmail.com>
2020-12-22 09:03:20 -0500

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(array => {
+   return array.split(' ').map(word => {
+     return word.charAt(0).toUpperCase() + word.slice(1)
+    }).join(' ')
+  })
 }
+
+
+
+
