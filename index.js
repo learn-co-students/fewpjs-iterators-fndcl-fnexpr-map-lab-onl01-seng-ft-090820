461f785a4434824ba1c844eb6402e1b7abe92d49
csdj92 <csdj92@gmail.com>
2020-12-22 10:12:09 -0500

    Done.

--- index.js
+++ index.js
@@ -11,13 +11,8 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials.map(array => {
-   return array.split(' ').map(word => {
-     return word.charAt(0).toUpperCase() + word.slice(1)
-    }).join(' ')
-  })
-}
+const titleCased = () => tutorials.map(array => array.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' '))
+
 
 
 
